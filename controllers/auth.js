30ef5a3a1c29ed8ab86a053d26da7807d06d1c83
Ba8Rp <supergamming.70@gmail.com>
2026-03-03 12:50:15 +0700

    Auth And Restaurant

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,12 +7,13 @@ const User = require("../models/User");
 // @access   Public
 exports.register = async (req, res, next) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, telephone, password, role } = req.body;
 
     // Create user
     const user = await User.create({
       name,
       email,
+      telephone,
       password,
       role,
     });
@@ -100,5 +101,20 @@ exports.getMe = async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: user
+  });
+};
+
+// @desc    Logout user / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
   });
 };
